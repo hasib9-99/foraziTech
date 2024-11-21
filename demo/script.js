500ca1bf2b4240948b52f354e73f078c05d22dfa
hasib9-99 <abuhasibg@gmail.com>
2024-11-21 13:21:40 +0600

    input type file name change

--- demo/script.js
+++ demo/script.js
@@ -16,3 +16,32 @@ function transform(section) {
   scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
 }
 
+// input type file name modify
+const fileContainer = document.querySelector('.wapf-field-file');
+const fileInput = fileContainer.querySelector('.wapf-input');
+
+const customLabel = document.createElement('label');
+customLabel.textContent = 'JPG, PNG, PSD and AI are accepted';
+customLabel.className = 'custom-file-label';
+
+const uniqueId = 'custom-file-input'; 
+fileInput.id = uniqueId;
+customLabel.setAttribute('for', uniqueId);
+
+const fileList = document.createElement('div');
+fileList.textContent = 'No files selected';
+fileList.className = 'selected-files';
+
+const inputContainer = fileInput.parentNode;
+inputContainer.appendChild(customLabel);
+customLabel.appendChild(fileList);
+
+fileInput.addEventListener('change', () => {
+  const files = fileInput.files;
+  if (files.length > 0) {
+    const fileNames = Array.from(files).map(file => file.name).join(', ');
+    fileList.textContent = `${fileNames}`;
+  } else {
+    fileList.textContent = 'No files selected';
+  }
+});
