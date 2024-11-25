a23b1e999717db1d4f02ad28d06adbe5c61f834c
hasib9-99 <abuhasibg@gmail.com>
2024-11-25 09:16:12 +0600

    From step nevigator

--- demo/script.js
+++ demo/script.js
@@ -15,3 +15,16 @@ function transform(section) {
   percentage = percentage < 0 ? 0 : percentage > 200 ? 200 : percentage;
   scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
 }
+
+//Form step nevigetor
+setTimeout(() => {
+  const Steps = document.querySelectorAll('.e-form__indicators__indicator');
+  const stepBtns = document.querySelectorAll('.e-form__buttons__wrapper');
+  const navigator = document.querySelector('.navigator h2')
+  stepBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const activeIndex = Array.from(Steps).findIndex(step => step.classList.contains('e-form__indicators__indicator--state-active'));
+      navigator.textContent = `${activeIndex + 1} / ${Steps.length}`
+    })
+  })
+}, 300);
