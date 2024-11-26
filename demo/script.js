da68d9960396a85b9861508e2e8414495c7a6a0a
hasib9-99 <abuhasibg@gmail.com>
2024-11-26 10:38:52 +0600

    UPDATE FORM STEP NEVIGATOR

--- demo/script.js
+++ demo/script.js
@@ -28,3 +28,46 @@ setTimeout(() => {
     })
   })
 }, 300);
+
+
+
+setTimeout(() => {
+  const inputField = document.getElementById('Field-cvcInput');
+  inputField.placeholder = '123';
+}, 500);
+
+
+
+//Update Form step nevigator
+setTimeout(() => {
+  const Steps = document.querySelectorAll('.e-form__indicators__indicator');
+  const stepBtns = document.querySelectorAll('.e-form__buttons__wrapper');
+  const customBackBtn = document.querySelector('.back_btn');
+  const stepBackBtns = document.querySelectorAll('.e-form__buttons__wrapper__button-previous');
+  const navigator = document.querySelector('.navigator h2');
+  
+  function FindIndex() {
+    return Array.from(Steps).findIndex(step =>
+      step.classList.contains('e-form__indicators__indicator--state-active') // find wich index active 
+    );
+  }
+  
+  function updateNevigatror() {
+    const activeIndex = FindIndex();
+    navigator.textContent = `Schritte ${activeIndex + 1} / ${Steps.length}`;
+  }
+  
+  stepBtns.forEach((btn) => {
+    btn.addEventListener('click', updateNevigatror);
+  });
+  
+  // Add event listener to the custom back button
+  customBackBtn.addEventListener('click', () => {
+    const activeIndex = FindIndex();
+    if (stepBackBtns[activeIndex] && activeIndex !== 0) {  // check frist index
+      console.log(activeIndex);
+      stepBackBtns[activeIndex].click();
+  
+    }
+  });
+}, 300);
